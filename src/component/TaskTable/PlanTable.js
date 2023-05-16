c3384d7be097b672931e1e7548a13f1d19fb64f9
ralph-p <mr.pereira91@gmail.com>
2023-05-16 11:45:13 -0400

    fixed sizing

--- src/component/TaskTable/PlanTable.js
+++ src/component/TaskTable/PlanTable.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Button, Box, Td, Link, IconButton, HStack, Th, Tr, Thead, TableContainer, Table, Text, Tbody } from '@chakra-ui/react';
+import { GridItem, Box, Td, Link, IconButton, Grid, Text, useMediaQuery } from '@chakra-ui/react';
 import { Icon } from "@chakra-ui/icons";
 import { getChildTreeLength } from '@/utils/constants';
 import moment from 'moment';
-import { dateFormatMonthDayTime } from '@/utils/formats';
+import { dateFormatMonthDayTime, dateFormatMonthDayTimeShort } from '@/utils/formats';
 import { FaExternalLinkAlt } from 'react-icons/fa'
 import { DeleteTaskIcon } from '../DeleteTaskIcon';
 import { TableControl } from './TableControl';
@@ -11,36 +11,33 @@ import { TableControl } from './TableControl';
 
 
 const TableElement = ({ node, elements, addTask = () => { }, deleteTask = () => { } }) => {
+  const [isLargeScreen] = useMediaQuery("(min-width: 800px)")
 
   return (
-    <Tr bg={node?.complete && 'brandCard.50'}>
-      <Td><Text as={Link} href={`/${node?.type}/${node?.id}`} >{node.name}</Text></Td>
-      <Td>{moment(node?.inserted_at).format(dateFormatMonthDayTime)}</Td>
-      <Td>{getChildTreeLength(node, elements)}</Td>
-      <Td>
+    <>
+      <GridItem bg={node?.complete && 'brandCard.50'} height={"4em"} display="flex" overflow={'hidden'}><Text as={Link} href={`/${node?.type}/${node?.id}`} >{node.name}</Text></GridItem>
+      <GridItem bg={node?.complete && 'brandCard.50'} height={"4em"} display="flex">{moment(node?.inserted_at).format(isLargeScreen ? dateFormatMonthDayTime : dateFormatMonthDayTimeShort)}</GridItem>
+      <GridItem bg={node?.complete && 'brandCard.50'} height={"4em"} display="flex">{getChildTreeLength(node, elements)}</GridItem>
+      <GridItem bg={node?.complete && 'brandCard.50'} height={"4em"} display="flex">
         <TableControl>
           <IconButton colorScheme='brand' aria-label='open-task-icon' size='sm' icon={<Icon as={FaExternalLinkAlt} boxSize={4} />} as={Link} href={`/${node?.type}/${node?.id}`} />
           <DeleteTaskIcon onDelete={() => deleteTask(node?.id)} taskName={node?.name} />
         </TableControl>
-      </Td>
-    </Tr>
+      </GridItem>
+    </>
   )
 }
 export const PlanTable = ({ taskTree, parentId, pageTask, deleteTask }) => {
   if (taskTree.length) {
     const rootElements = taskTree.filter(el => el.parent_id === parentId);
     return (
-      <TableContainer color={'brand.50'}>
-        <Table variant='simple'>
-          <Thead>
-            <Tr>
-              <Th>Name</Th>
-              <Th>Created</Th>
-              <Th>No. Activities</Th>
-              <Th>Control</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
+      <Box color={'brand.50'}>
+        <Grid templateColumns='repeat(4, 1fr)' rowGap={1}>
+          <GridItem><Text fontWeight='bold'>Name</Text> </GridItem>
+          <GridItem><Text fontWeight='bold'>Created</Text></GridItem>
+          <GridItem><Text fontWeight='bold'>No. Activities</Text></GridItem>
+
+          <GridItem><Text fontWeight='bold'>Control</Text></GridItem>
 
             {rootElements.map((root) => (
               <TableElement
@@ -49,9 +46,8 @@ export const PlanTable = ({ taskTree, parentId, pageTask, deleteTask }) => {
                 elements={taskTree}
                 deleteTask={deleteTask}
               />))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+        </Grid>
+      </Box>
     )
   }
 }
