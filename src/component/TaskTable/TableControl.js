01dcecf12d18070fa5c13bdf229f73c47f86ef2f
ralph-p <mr.pereira91@gmail.com>
2023-05-16 10:38:15 -0400

    added responsive size

--- src/component/TaskTable/TableControl.js
+++ src/component/TaskTable/TableControl.js
@@ -1,23 +1,28 @@
-import { HStack, Popover, PopoverTrigger, PopoverContent, Box, PopoverArrow } from '@chakra-ui/react'
+import { HStack, Popover, PopoverTrigger, PopoverContent, Box, PopoverArrow, useMediaQuery } from '@chakra-ui/react'
 import { Icon } from "@chakra-ui/icons";
 
 import {TbDots} from 'react-icons/tb'
 import React from 'react'
 
 export const TableControl = ({ children }) => {
+  const [isLargeScreen] = useMediaQuery("(min-width: 800px)")
+  if (isLargeScreen) {
+    return <HStack spacing={2}>{children}</HStack>;
+  }
   return (
-    <Popover>
+    <Popover placement='left'>
       <PopoverTrigger>
-        <Icon as={TbDots}/>
+        <Box cursor={'pointer'}>
+
+        <Icon as={TbDots} boxSize={5}/>
+        </Box>
       </PopoverTrigger>
       <PopoverArrow />
-      <PopoverContent color='white' bg='blue.800' borderColor='blue.800' padding={'1'}>
-        <HStack>
-
+      <PopoverContent color='white' bg='brand.600' borderColor='brand.500' padding={2} width='full' borderWidth={2}>
+        <HStack spacing={2}>
           {children}
         </HStack>
       </PopoverContent>
     </Popover>
   )
 }
-
